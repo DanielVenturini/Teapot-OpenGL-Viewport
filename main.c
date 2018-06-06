3b90fde021458dc0e88e2119515abde826409234
Daniel Venturini <daniellagarto0@gmail.com>
2018-06-06 12:34:17 -0300

    Tentando adicionar viewport

--- main.c
+++ main.c
@@ -10,33 +10,42 @@ int init(void){
     glClearColor(1.0, 1.0, 1.0, 0.0);   // define a cor de fundo
     glEnable(GL_DEPTH_TEST);            /// habilitao teste de profundidade
 
-    glMatrixMode(GL_MODELVIEW);         // carrega a matriz de projeção
+    //glMatrixMode(GL_MODELVIEW);         // carrega a matriz de projeção
+    glMatrixMode(GL_PROJECTION);            // carrega a matriz de projeção
     glLoadIdentity();                   /// carrega a matrix de identidade
     //gluOrtho2D(0.0,100.0,0.0,100.0);  // define projeção ortogonal 2D
+
+    /*gluLookAt(4.0, 2.0, 2.0,    //posição da câmera
+              0.0, 0.0, 0.0,    //para onde a câmera aponta
+              0.0, 1.0, 0.0);   //vetor view*/
+
+    //glMatrixMode(GL_PROJECTION);         // carrega a matriz de projeção
+    //glLoadIdentity();
+    //gluPerspective(45.0, 1.0, 0.1, 10.0);
+    glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);   /// definindo a janela de recorte
+    glMatrixMode(GL_MODELVIEW);         // carrega a matriz de projeção
+    glLoadIdentity();
+
+    glColor3f(1.0f, 0.0f, 0.0f);            //alterao atributo de cor
+
+    /*criaViewPortProjecao(0, 800, 400, 400);
+    criaViewPortProjecao(0, 400, 400, 0);
+    criaViewPortProjecao(400, 400, 800, 800);
+    criaViewPortProjecao(400, 400, 800, 0);*/
+
+}
+
+void criaViewPortProjecao(int p1, int p2, int p3, int p4){
+    glViewport(p1, p2, p3, p4);     /// define a primeira viewport
+    glOrtho(-3, 3, -3, 3, 1, 50);   /// projecao ortogonal
+}
+
+void posicaoCamera(){
 
     gluLookAt(4.0, 2.0, 2.0,    //posição da câmera
               0.0, 0.0, 0.0,    //para onde a câmera aponta
               0.0, 1.0, 0.0);   //vetor view
 
-    glMatrixMode(GL_PROJECTION);         // carrega a matriz de projeção
-    glLoadIdentity();
-    gluPerspective(45.0, 1.0, 0.1, 10.0);
-    //glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);   /// definindo a janela de recorte
-}
-
-void desenha_objeto() {
-    glBegin(GL_TRIANGLES);      // desenha um t r i a n g u l o
-        glVertex2i(50, -50);
-        glVertex2i(0, 50);
-        glVertex2i(-50, -50);
-    glEnd();
-
-    glBegin(GL_LINE_LOOP);      // desenha um quadrado
-        glVertex2i(-99, -99);
-        glVertex2i(99, -99);
-        glVertex2i(99, 99);
-        glVertex2i(-99, 99);
-    glEnd();
 }
 
 void criaProjecoesOrtogonais(){
@@ -51,40 +60,11 @@ void display(void){
     glClear(GL_COLOR_BUFFER_BIT |  GL_DEPTH_BUFFER_BIT);    // desenha o fundo (limpa a janela)
     glMatrixMode(GL_MODELVIEW);     /// carrega a matriz modelo
 
-     // desenha  um  cubo
+     // desenha  um  TeaPot
     glColor3f(1.0f, 0.0f, 0.0f);
     glutWireTeapot(1.0f);
 
-    /*
-    glColor3f(0.0, 0.0, 0.0);       /// altera a cor do fundo para branco
-    glLoadIdentity();               // carrega a matriz identidade
-
-    glViewport(-3, 3, -3, 3);       /// view port superior esquerdo
-    glColor3f(1.0, 0.0, 0.0);       // define a cor de fundo
-    desenha_objeto();
-
-    glViewport(-3, 3, -3, 3);       /// view port superior direito
-    glColor3f(0.0, 1.0, 0.0);       // define a cor de fundo
-    desenha_objeto();
-
-    glViewport(-3, 3, -3, 3);       /// view port inferior esquerdo
-    glColor3f(0.0, 0.0, 1.0);       // define a cor de fundo
-    desenha_objeto();
-
-    /*
-    glTranslatef(tx+(tamQuad/2), ty+(tamQuad/2), 0);
-    glRotatef(theta, 0, 0, 1);
-    glTranslatef(-tx-(tamQuad/2), -ty-(tamQuad/2), 0);
-
-    glBegin(GL_POLYGON);                    // desenha uma linha
-        glVertex2i(tx, ty);
-        glVertex2i(tx, ty + tamQuad);
-        glVertex2i(tx + tamQuad, ty + tamQuad);
-        glVertex2i(tx + tamQuad, ty);
-    glEnd();
-    */
-
-    glFlush();                            // desenha os comandos não executados
+    glFlush();      // desenha os comandos não executados
 }
 
 int main(int argc, char** argv) {
